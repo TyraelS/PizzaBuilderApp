328c8f9dd6d2ac07206e016a9d072a69b0253c99
TyraelS <oshynkarenko1503@gmail.com>
2018-09-11 20:23:37 +0300

    PizzaIngredient Component implement

---
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxil';
+
+export default class PizzaBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
+}
